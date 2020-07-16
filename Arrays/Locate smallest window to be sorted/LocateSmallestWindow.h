a6677b621a06e6fdd3e561a2a0952e1dece58c78
Cohl Dorsey <cohldorsey@gmail.com>
2020-07-16 01:26:09 -0500

    Wrote down the problem and started the header file for the class that will be made.

---
+++ Arrays/Locate smallest window to be sorted/LocateSmallestWindow.h
@@ -0,0 +1,17 @@
+#ifndef LOCATESMALLESTWINDOW_H
+#define LOCATESMALLESTWINDOW_H
+
+#include <iostream>
+#include <array>
+
+class LocateSmallestWindow
+{
+public:
+    array findWindow(array integerList);
+
+private:
+    array _integerList;
+};
+
+
+#endif // LOCATESMALLESTWINDOW_H
